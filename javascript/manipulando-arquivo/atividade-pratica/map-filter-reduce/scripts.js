a772532f31f3531b1271f0c3cddf735a8671806d
Thiago <thicef@gmail.com>
2022-01-05 13:59:36 -0300

    Create scripts.js

---
+++ javascript/manipulando-arquivo/atividade-pratica/map-filter-reduce/scripts.js
@@ -0,0 +1,57 @@
+/*
+const maca = {
+    value: 2,
+};
+
+const laranja = {
+    value: 3,
+};
+
+function mapComThis(arr, thisArg) {
+    return arr.map(function (item) {
+       return item * this.value;
+    }, thisArg);
+}
+
+const nums = [1, 2];
+
+console.log('this -> maçã', mapComThis(nums, maca));
+
+console.log('this -> laranja', mapComThis(nums, laranja));
+*/
+
+
+/*
+function mapSemThis(arr) {
+    return arr.map(function(item){
+        return item * 2;
+    });
+}
+
+const nums = [2, 4, 6, 8, 10];
+
+console.log(mapSemThis(nums));
+*/
+
+
+/*
+function filtraPares(arr) {
+    return arr.filter (callback)
+}
+function callback(item) {
+    return item % 2 === 0;
+}
+const meuArray = [1, 23, 55, 67, 30, 2, 4];
+
+console.log(filtraPares(meuArray));
+*/
+
+function somaNumeros(arr) {
+    return arr.reduce(function(prev, current) {
+        return prev + current;
+    })
+}
+
+const arr = [1, 2];
+
+console.log(somaNumeros(arr));
