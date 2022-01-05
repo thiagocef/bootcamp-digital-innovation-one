88a68ffb8562308b1dc1d718ad8e5d4ad5c39995
Thiago <thicef@gmail.com>
2022-01-05 14:13:03 -0300

    atividade pratica
    map, filter, reduce

--- javascript/manipulando-arquivo/atividade-pratica/map-filter-reduce/scripts.js
+++ javascript/manipulando-arquivo/atividade-pratica/map-filter-reduce/scripts.js
@@ -46,8 +46,11 @@ const meuArray = [1, 23, 55, 67, 30, 2, 4];
 console.log(filtraPares(meuArray));
 */
 
+/*
 function somaNumeros(arr) {
     return arr.reduce(function(prev, current) {
+        console.log({prev});
+        console.log({current})
         return prev + current;
     })
 }
@@ -55,3 +58,36 @@ function somaNumeros(arr) {
 const arr = [1, 2];
 
 console.log(somaNumeros(arr));
+*/
+
+
+/*
+const lista = [
+    {
+        name: 'sabão em pó',
+        preco: 30,
+    },
+    {
+        name: 'cereal',
+        preco: 12,
+    },
+    {
+        name: 'toalha',
+        preco: 30,
+    },
+
+];
+const saldoDisponivel = 100;
+
+function calculaSaldo(saldoDisponivel, lista) {
+    return lista.reduce(function(prev, current, index) {
+        console.log('rodada', index + 1);
+        console.log({ prev });
+        console.log({ current });
+        return prev - current.preco;
+    }, saldoDisponivel);
+}
+
+console.log(calculaSaldo(saldoDisponivel, lista));
+*/
+
